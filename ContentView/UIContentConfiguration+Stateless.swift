1ccb028f1763f3e23fbac256334856ef12b5a0b5
tengkuzulfadli <nomad.plus@yahoo.com>
2022-11-13 15:28:13 +1100

    Date and Notes edit views implementation

---
+++ ContentView/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  TodayReminder
+//
+//  Created by Tengku Zulfadli on 13/11/2022.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
